5542f52081f654cc42e9d652de4d74e4497e67ff
AtharvChaphalkar <atharv.chaphalkar@gmail.com>
2024-06-04 10:19:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,20 @@ let slideIndex = 0;
 showSlides();
 
 function showSlides() {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+    let slides = document.getElementsByClassName("slides");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
     }
     slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
-    slides[slideIndex-1].style.display = "block";
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    slides[slideIndex-1].style.display = "block";  
     setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+function navigateTo(sectionId) {
+    let sections = document.getElementsByClassName("info-section");
+    for (let section of sections) {
+        section.classList.remove("active");
+    }
+    document.getElementById(sectionId).classList.add("active");
 }
